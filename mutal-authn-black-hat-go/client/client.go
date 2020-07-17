29896f31034ba35f4553717b17c25eff3a3a40fb
Tianri Gu <tianrigu@Tmac.local>
2020-07-16 22:20:04 -0700

    Add TCP TLS listener and client

--- mutal-authn-black-hat-go/client/client.go
+++ mutal-authn-black-hat-go/client/client.go
@@ -19,33 +19,17 @@ const (
 var (
 	serverAddr string
 	serverPort string
+	proto string
 )
 
 func init() {
 	flag.StringVar(&serverAddr, "addr", "", "address to listen for HTTPS requests")
 	flag.StringVar(&serverPort, "port", "443", "port for TLS connection")
+	flag.StringVar(&proto, "proto", "HTTPS", "protocol to serve, TCP or HTTPS")
 	flag.Parse()
 }
 
-func main() {
-	cert, err := tls.LoadX509KeyPair(clientCertFile, clientKeyFile)
-	if err != nil {
-		log.Fatalln(err)
-	}
-
-	serverCert, err := ioutil.ReadFile(serverCertFile)
-	if err != nil {
-		log.Fatalln(err)
-	}
-
-	pool := x509.NewCertPool()
-	pool.AppendCertsFromPEM(serverCert)
-
-	tlsConfig := &tls.Config{
-		Certificates: []tls.Certificate{cert},
-		RootCAs: pool,
-	}
-
+func tryHTTPS(tlsConfig *tls.Config) {
 	transport := &http.Transport{
 		TLSClientConfig: tlsConfig,
 	}
@@ -68,3 +52,55 @@ func main() {
 
 	log.Infof("Success: %s", body)
 }
+
+func tryTCP(tlsConfig *tls.Config) {
+	conn, err := tls.Dial("tcp", serverAddr+":"+serverPort, tlsConfig)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer conn.Close()
+	log.Info("connected to: ", conn.RemoteAddr())
+
+	state := conn.ConnectionState()
+	log.Debugf("server CNAME: %s", state.PeerCertificates[0].Subject.CommonName)
+	log.Debug("client: handshake: ", state.HandshakeComplete)
+	log.Debug("client: mutual: ", state.NegotiatedProtocolIsMutual)
+
+	reply := make([]byte, 256)
+	n, err := conn.Read(reply)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	log.Infof("Got: %q (%d bytes)", string(reply[:n]), n)
+}
+
+func main() {
+	cert, err := tls.LoadX509KeyPair(clientCertFile, clientKeyFile)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	serverCert, err := ioutil.ReadFile(serverCertFile)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	pool := x509.NewCertPool()
+	pool.AppendCertsFromPEM(serverCert)
+
+	tlsConfig := &tls.Config{
+		Certificates: []tls.Certificate{cert},
+		RootCAs: pool,
+	}
+
+	switch proto {
+	case "HTTPS":
+		tryHTTPS(tlsConfig)
+	case "TCP":
+		tryTCP(tlsConfig)
+	default:
+		log.Error("Unknown protocol, exit...")
+	}
+}
+
+// go run client.go -addr localhost -port 9000
--- mutal-authn-black-hat-go/server/server.go
+++ mutal-authn-black-hat-go/server/server.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 	log "github.com/sirupsen/logrus"
 	"io/ioutil"
+	"net"
 	"net/http"
 )
 
@@ -20,6 +21,7 @@ const (
 var (
 	listenAddr string
 	listenPort string
+	proto string
 )
 
 func helloHandler(w http.ResponseWriter, r *http.Request) {
@@ -27,16 +29,57 @@ func helloHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "AuthN successfully: ", r.RemoteAddr)
 }
 
+func helloHandlerTCP(conn net.Conn) {
+	defer conn.Close()
+	tlsConn, ok := conn.(*tls.Conn)
+	if ok {
+		err := tlsConn.Handshake()
+		if err != nil {
+			log.Fatalln(err)
+		}
+		state := tlsConn.ConnectionState()
+		log.Infof("Hello %#v", state.PeerCertificates[0].Subject.CommonName)
+		fmt.Fprintln(conn, "AuthN successfully: ", conn.RemoteAddr())
+	}
+}
+
 func init() {
 	flag.StringVar(&listenAddr, "addr", "", "address to listen for HTTPS requests")
 	flag.StringVar(&listenPort, "port", "443", "port for TLS connection")
+	flag.StringVar(&proto, "proto", "HTTPS", "protocol to serve, TCP or HTTPS")
 	flag.Parse()
 }
 
-func main() {
-	log.Infof("HTTPS server started on %s:%s", listenAddr, listenPort)
+func serveTCP(tlsConfig *tls.Config) {
+	listener, err := tls.Listen("tcp", listenAddr+":"+listenPort, tlsConfig)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	log.Infof("TCP server started on %s:%s", listenAddr, listenPort)
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Fatalln(err)
+		}
+		go helloHandlerTCP(conn)
+	}
+}
 
+func serveHTTPS(tlsConfig *tls.Config) {
 	http.HandleFunc("/hello", helloHandler)
+	server := &http.Server{
+		Addr: listenAddr+":"+listenPort,
+		TLSConfig: tlsConfig,
+	}
+	log.Infof("HTTPS server started on %s:%s", listenAddr, listenPort)
+	log.Fatalln(server.ListenAndServeTLS("", ""))
+}
+
+func main() {
+	serverCert, err := tls.LoadX509KeyPair(serverCertFile, serverKeyFile)
+	if err != nil {
+		log.Fatalln(err)
+	}
 
 	clientCert, err := ioutil.ReadFile(clientCertFile)
 	if err != nil {
@@ -49,11 +92,17 @@ func main() {
 	tlsConfig := &tls.Config{
 		ClientCAs: pool,
 		ClientAuth: tls.RequireAndVerifyClientCert,
+		Certificates: []tls.Certificate{serverCert},
 	}
 
-	server := &http.Server{
-		Addr: listenAddr+":"+listenPort,
-		TLSConfig: tlsConfig,
+	switch proto {
+	case "HTTPS":
+		serveHTTPS(tlsConfig)
+	case "TCP":
+		serveTCP(tlsConfig)
+	default:
+		log.Error("Unknown protocol, exit...")
 	}
-	log.Fatalln(server.ListenAndServeTLS(serverCertFile, serverKeyFile))
 }
+
+// go run server.go -addr localhost -port 9000 -proto TCP
